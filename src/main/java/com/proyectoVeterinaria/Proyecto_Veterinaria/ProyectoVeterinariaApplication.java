00da6e33fa1fef4b3b53b0d9bc8fb02900bedd51
rodrirs7 <rodrios334@gmail.com>
2021-10-27 19:49:47 -0300

    Comentario prueba de rodri

--- src/main/java/com/proyectoVeterinaria/Proyecto_Veterinaria/ProyectoVeterinariaApplication.java
+++ src/main/java/com/proyectoVeterinaria/Proyecto_Veterinaria/ProyectoVeterinariaApplication.java
@@ -8,6 +8,7 @@ public class ProyectoVeterinariaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoVeterinariaApplication.class, args);
+//                comentario prueba Rodri-
 	}
 
 }
