326e89bd3d148b014dc667e5f14e0f16c9c58dd4
94MartinAlejandroQuiroz <91095246+94MartinAlejandroQuiroz@users.noreply.github.com>
2021-11-01 21:33:52 -0300

    martin cambio repo client, service cli noti profe

--- src/main/java/com/proyectoVeterinaria/Proyecto_Veterinaria/Servicios/NotificacionServicio.java
+++ src/main/java/com/proyectoVeterinaria/Proyecto_Veterinaria/Servicios/NotificacionServicio.java
@@ -1,13 +1,25 @@
 package com.proyectoVeterinaria.Proyecto_Veterinaria.Servicios;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificacionServicio {
     
+    @Autowired
+    private JavaMailSender mailSender;
+    
     @Async
     public void enviar(String cuerpo, String titulo, String mail){
-        
+        SimpleMailMessage mensaje = new SimpleMailMessage();
+        mensaje.setTo(mail);
+        mensaje.setFrom("noreply@ VETERINARIA .com"); //decidir nombre de la VETE
+        mensaje.setSubject(titulo);
+        mensaje.setText(cuerpo);
+
+        mailSender.send(mensaje);
     }
 }
--- src/main/java/com/proyectoVeterinaria/Proyecto_Veterinaria/Servicios/ProfesionalServicio.java
+++ src/main/java/com/proyectoVeterinaria/Proyecto_Veterinaria/Servicios/ProfesionalServicio.java
@@ -4,14 +4,24 @@ import com.proyectoVeterinaria.Proyecto_Veterinaria.Entidades.Profesional;
 import com.proyectoVeterinaria.Proyecto_Veterinaria.Entidades.Usuario;
 import com.proyectoVeterinaria.Proyecto_Veterinaria.Enumeraciones.EnumRol;
 import com.proyectoVeterinaria.Proyecto_Veterinaria.Errores.ErrorServicio;
+import com.proyectoVeterinaria.Proyecto_Veterinaria.Repositorio.ProfesionalRepositorio;
+import com.proyectoVeterinaria.Proyecto_Veterinaria.Repositorio.UsuarioRepositorio;
+import java.util.Optional;
 import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfesionalServicio {
 
+    @Autowired
+    private ProfesionalRepositorio profesionalRepositorio;
+
+    @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
+
     @Transactional
-    public void registrar(String nombre, String apellido, Long telefono, EnumRol rol)throws ErrorServicio {
+    public void registrar(String nombre, String apellido, Long telefono, EnumRol rol, Usuario usuario) throws ErrorServicio {
         if (nombre == null || nombre.isEmpty()) {
             throw new ErrorServicio("El nombre del usuario no puede ser nulo");
         }
@@ -21,19 +31,36 @@ public class ProfesionalServicio {
         if (telefono == null) {
             throw new ErrorServicio("El número del telefono no puede ser nulo");
         }
-        
+
         Profesional profesional = new Profesional();
         profesional.setNombre(nombre);
         profesional.setApellido(apellido);
         profesional.setTelefono(telefono);
         profesional.setRol(rol);
-        
-        Usuario usuario =new Usuario();
-        
+        profesional.setIdUsuario(usuario);
+
+        profesionalRepositorio.save(profesional);
     }
 
     @Transactional
-    public void modificar() {
+    public void modificar(String id, String nombre, String apellido, Long telefono, EnumRol rol, Usuario usuario) {
+        Optional<Profesional> respuesta = profesionalRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+            Profesional profesional = respuesta.get();
+            if (profesional.getId().equals(id)) {
+                profesional.setNombre(nombre);
+                profesional.setApellido(apellido);
+                profesional.setTelefono(telefono);
+                profesional.setRol(rol);
+                profesional.setIdUsuario(usuario);
+                profesionalRepositorio.save(profesional);
+            } else {
+                throw new ErrorServicio("No tiene permisos suficientes para realizar la operacion");
+            }
+
+        } else {
+            throw new ErrorServicio("No existe ese id de profesional");
+        }
     }
 
     public void validar() {
@@ -44,6 +71,15 @@ public class ProfesionalServicio {
 
     @Transactional
     public void eliminar(String id) {
+        Optional<Profesional> respuesta = profesionalRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+            Profesional profesional = respuesta.get();
+            if (profesional.getIdUsuario().equals(id)) {
+                profesionalRepositorio.deleteById(id);
+            } else {
+                throw new ErrorServicio("No existe ese profesional");
+            }
+        }
     }
 
 }
